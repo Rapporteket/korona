e8ab83d9aa24958ffe5a30701c2c296f7152034c
kevinthon <kevin.thon@gmail.com>
2020-04-02 11:00:39 +0200

    reshnivåting

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,8 +1,10 @@
 ## code to prepare `DATASET` dataset goes here
 
 
-ReshNivaa <- read.table('data-raw/EnhetsnivaaerResh.csv', sep=';',
-                        stringsAsFactors=FALSE, header=T, encoding = 'UTF-8')
+# ReshNivaa <- read.table('data-raw/EnhetsnivaaerResh.csv', sep=';',
+#                         stringsAsFactors=FALSE, header=T, encoding = 'UTF-8')
+ReshNivaa <- read.table(system.file(file.path('extdata', 'EnhetsnivaaerResh.csv'), package = 'korona'), sep=';',
+                        stringsAsFactors=FALSE, header=T, fileEncoding = 'latin1')
 
 usethis::use_data(ReshNivaa, overwrite = TRUE, internal = TRUE)
 
