020890e963ed80babca5b42d24ae767bb9a992e5
kevinthon <kevin.thon@gmail.com>
2020-04-02 11:53:58 +0200

    Oppdatert ReshNivaa.rda med riktig encoding. RHFresh og HFresh legges til i preprosessering.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -6,7 +6,7 @@
 ReshNivaa <- read.table(system.file(file.path('extdata', 'EnhetsnivaaerResh.csv'), package = 'korona'), sep=';',
                         stringsAsFactors=FALSE, header=T, fileEncoding = 'latin1')
 
-usethis::use_data(ReshNivaa, overwrite = TRUE, internal = TRUE)
+usethis::use_data(ReshNivaa, overwrite = TRUE, internal = FALSE)
 
 #IndBeskr <- readxl::read_excel("data-raw/Indikatorbeskrivelser.xlsx")
 #usethis::use_data(IndBeskr, overwrite = TRUE)
