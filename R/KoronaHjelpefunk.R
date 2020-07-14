03d0d9571f4c0afb5254e8295438777a03b602b3
Lena Ringstad Olsen <lenaringstado@gmail.com>
2020-07-14 11:56:32 +0200

    testing

--- R/KoronaHjelpefunk.R
+++ R/KoronaHjelpefunk.R
@@ -119,8 +119,10 @@ sendDataFilerFHI <- function(zipFilNavn='Testfil', brukernavn = 'testperson'){ #
 
     raplog::subLogger(author = brukernavn, registryName = 'Pandemi', reshId = 0,
                       msg = paste0("Har lagret testfiler"))
-    print(kat)
-    utils::zip(zipfile = paste0(kat, '/', zipFilNavn), files = c(paste0(kat, '/Testfil1.csv'), paste0(kat, '/Testfil2.csv')))
+    #src <- normalizePath(system.file(rnwFil, package=Rpakke))
+    #utils::zip(zipfile = paste0(kat, '/', zipFilNavn), files = c(paste0(kat, '/Testfil1.csv'), paste0(kat, '/Testfil2.csv')))
+    #file.path()
+    utils::zip(zipfile = paste0(kat, '/', zipFilNavn), files = c(file.path(kat, 'Testfil1.csv'), file.path(kat, 'Testfil2.csv')))
 
 
     #file.info(c(paste0(zipFilNavn, '.zip'), 'Testfil1.csv', 'Testfil2.csv'))['size']
