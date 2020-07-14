2382bc40183964cb799c6534354d6b70780b94f0
Lena Ringstad Olsen <lenaringstado@gmail.com>
2020-07-14 12:09:12 +0200

    byttet om på zipfilsti

--- R/KoronaHjelpefunk.R
+++ R/KoronaHjelpefunk.R
@@ -123,7 +123,7 @@ sendDataFilerFHI <- function(zipFilNavn='Testfil', brukernavn = 'testperson'){ #
     #utils::zip(zipfile = paste0(kat, '/', zipFilNavn), files = c(paste0(kat, '/Testfil1.csv'), paste0(kat, '/Testfil2.csv')))
     #file.path()
     zipfilSti <- paste0(kat, '/', zipFilNavn, '.zip')
-    write.table(zipfilSti, file = 'zipfilSti.csv',fileEncoding = 'UTF-8')
+    write.table(zipfilSti, file = 'zipfilSti.csv', fileEncoding = 'UTF-8', row.names=F, sep=';', na='')
     utils::zip(zipfile = file.path(kat, zipFilNavn), files = c(file.path(kat, 'Testfil1.csv'), file.path(kat, 'Testfil2.csv')))
 
 
