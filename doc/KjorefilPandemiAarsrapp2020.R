9554b4ef72fbebe0dd40d9ed24f3d46d7c35811c
Lena Ringstad Olsen <lenaringstado@gmail.com>
2021-06-15 12:59:13 +0200

    fil

--- doc/KjorefilPandemiAarsrapp2020.R
+++ doc/KjorefilPandemiAarsrapp2020.R
@@ -34,36 +34,63 @@ datoFra1aar <- '2020-01-01'
 KoroDataRaa <- KoronaDataSQL(datoTil = datoTil)
 KoroDataPre <- KoronaPreprosesser(RegData = KoroDataRaa[KoroDataRaa$ArsakInnleggelse==1, ], aggPers = 0)
 KoroDataAllePers <- KoronaPreprosesser(RegData = KoroDataRaa, aggPers = 1)
-length(unique(KoroDataRaa$PersonId))
-length(unique(KoroDataPre$PersonId))
+#length(unique(KoroDataRaa$PersonId))
+#length(unique(KoroDataPre$PersonId))
 
 BeredDataRaa <- NIRberedskDataSQL()
 BeredData <- NIRPreprosessBeredsk(RegData=BeredDataRaa, aggPers = 0)
-
+BeredDataPers <- NIRPreprosessBeredsk(RegData=BeredDataRaa, aggPers = 1)
 #Kobler pandemi og beredskap:
+persBered <- unique(BeredDataRaa$PersonId)
+persPandemi <- unique(KoroDataRaa$PersonId)
+persBeredUpandemi <- setdiff(persBered, persPandemi)
+beredRegUpan <- BeredDataRaa[which(BeredDataRaa$PersonId %in% persBeredUpandemi),
+                             c("PersonId", "SkjemaGUID", "ShNavn", "RHF")]
+write.table(beredRegUpan, file = 'BeredRegUpan.csv', row.names = F, sep = ';')
 #FEIL!!! Kan bare brukes for personaggregerte data. Må også ta hensyn til innleggelsestidspunkt.
 # KoroData <- merge(KoroDataPre, BeredData, all.x = T, all.y = F,
 #                   suffixes = c("", "Bered"), by = 'PersonId')
 
 #Hvilke pandemiskjema har beredskapsskjema?
+#Koble personaggregerte data for sjekk
+KoblPers <- merge(KoroDataAllePers[which(KoroDataAllePers$Nir_beredskapsskjema_CoV2==1) ,c('PasientID', 'PersonId', 'Nir_beredskapsskjema_CoV2')],
+                  BeredDataPers[ ,c('PasientID', 'PersonId')],
+              all.x = T, all.y = F, by = 'PersonId', suffixes = c("", "Bered"))
+KoblPers <- merge(KoroDataAllePers[ ,c('PasientID', 'PersonId')], BeredDataPers[ ,c('PasientID', 'PersonId')],
+                  all.x = F, all.y = T, by = 'PersonId', suffixes = c("", "Bered"))
+sum(!is.na(KoblPers$PasientID))
+
+KoroDataPre$SkjemaGUIDBered <- as.character(BeredData$SkjemaGUID[match(KoroDataPre$PersonId, BeredData$PersonId)])
+KoroDataPre$BeredPas <- !is.na(KoroDataPre$SkjemaGUIDBered)
+table(table(KoroDataPre$SkjemaGUIDBered))
+sum(!is.na(KoroDataPre$SkjemaGUIDBered))
+length(unique(KoroDataPre$PersonId[!is.na(KoroDataPre$SkjemaGUIDBered)]))
+#RegData$HFkort <- as.character(HFmap$HFnavn[match(RegData$HFresh, HFmap$HFresh)])
+#Kan bare koble på personid. Flere personer har mer enn ett skjerma. Får derfor ikke entydig kobling av
+#hvilket koronaopphold som har intensivopphold.
 Kobl <- merge(KoroDataPre[,c("PersonId","SkjemaGUID")], #, "InnTidspunkt", "UtTidspunkt" )],
               BeredData[,c("PersonId", "SkjemaGUID")], #"Innleggelsestidspunkt", "DateDischargedIntensive")],
-              all.x = T, all.y = F,
+              all.x = T, all.y = F, # For å få med alle pandemiskjema
               suffixes = c("", "Bered"), by = 'PersonId')
+sum(!is.na(unique(Kobl$SkjemaGUIDBered)))
+sort(table(Kobl$SkjemaGUIDBered))
 KoblRed <- Kobl %>% group_by(SkjemaGUID) %>%
   summarise(SkjemaGUID = SkjemaGUID[1],
+            AntBered = sum(!is.na(SkjemaGUIDBered)),
     BeredPas = ifelse(sum(!is.na(SkjemaGUIDBered))>0 ,1 ,0)
     )
+sort(table(KoblRed$AntBered))
+
 #KoroData  <- KoroData %>% mutate(BeredPas = ifelse(is.na(PasientIDBered), 0, 1))
 
 KoroData <- merge(KoroDataPre, KoblRed, by = 'SkjemaGUID')
 
 #Får vi nå et intensivopphold for alle skjema som tilhører en pasient som har hatt intensivopphold?
+persInt <- length(unique(BeredData$PersonId))
 pers <- unique(KoroData$PersonId[which(KoroData$BeredPas==1)])
 length(pers)
 test <- KoroData[which(KoroData$PersonId %in% pers), c("PersonId", "SkjemaGUID", "BeredPas")] #Har alle disse beredskapsopphold på hvert skjema
 table(test$BeredPas)
-table(table(KoroDataPre$))
 table(KoroData$BeredPas)
 #Skjekk ved å sjekke antall beredskapsskjema som er knyttet opp. TEll også hvor mange beredskapsskjema hver pasient har og om
 
